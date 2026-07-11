4171d73bdfbd7ec36b365c43849607908b67afc5
ahmedprogriming <ahmedjuid5@gmail.com>
2026-07-11 17:25:41 +0300

    create custom app bar and create notes view body

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:ui_web';
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/notes_view.dart';
 
---
+++ lib/widget/custom_app_bar.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+import 'package:notes_app/widget/custom_searchIcon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Text('Notes',style: TextStyle(fontSize: 28),),
+        Spacer(),
+        CustomSearchIcon()
+      ],
+    );
+  }
+}
+
