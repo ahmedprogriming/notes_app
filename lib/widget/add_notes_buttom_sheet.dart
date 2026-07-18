d5e97194ab55b901f5dd9a2481f3813e071dc45d
ahmedprogriming <ahmedjuid5@gmail.com>
2026-07-18 22:15:15 +0300

    finsh add note bottom sheet

--- lib/widget/add_notes_buttom_sheet.dart
+++ lib/widget/add_notes_buttom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constant.dart';
 import 'package:notes_app/widget/custom_Text_field.dart';
 
 class AddNoteShowButtomSheet extends StatelessWidget {
@@ -8,11 +9,37 @@ class AddNoteShowButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 30,),
-          CustomTextFiled()
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 30,),
+            CustomTextFiled(hint: 'Title',),    
+               SizedBox(height: 16,),
+            CustomTextFiled(hint: 'Content',maxLines: 5,),
+             SizedBox(height: 16,),
+            CustomButton(),
+             SizedBox(height: 16,),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+        color:kPrimaryColor,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: const Center(
+        child: Text('Add',style: TextStyle(color: Colors.black,fontSize: 20),),
       ),
     );
   }
--- lib/widget/custom_Text_field.dart
+++ lib/widget/custom_Text_field.dart
@@ -2,14 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constant.dart';
 
 class CustomTextFiled extends StatelessWidget {
-  const CustomTextFiled({super.key});
+  const CustomTextFiled({super.key, this.hint, this.maxLines=1});
+
+  final String? hint;
+  final int? maxLines;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-        
+      cursorColor: kPrimaryColor,
+      //textAlign: TextAlign.center,
+        textAlignVertical: TextAlignVertical.center,
+      maxLines: maxLines,
       decoration: InputDecoration(
-        hintText: 'Title',
+        hintText: hint,
         hintStyle: TextStyle(color: kPrimaryColor),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(16),
