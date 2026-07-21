c93e3b709b60e23e3f9e388da035b5e382587ab2
ahmedprogriming <ahmedjuid5@gmail.com>
2026-07-21 17:24:52 +0300

    take input and valdate data

--- lib/widget/add_notes_buttom_sheet.dart
+++ lib/widget/add_notes_buttom_sheet.dart
@@ -1,45 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/constant.dart';
 import 'package:notes_app/widget/custom_Text_field.dart';
+import 'package:notes_app/widget/custom_button.dart';
 
 class AddNoteShowButtomSheet extends StatelessWidget {
   const AddNoteShowButtomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 30,),
-            CustomTextFiled(hint: 'Title',),    
-               SizedBox(height: 16,),
-            CustomTextFiled(hint: 'Content',maxLines: 5,),
-             SizedBox(height: 16,),
-            CustomButton(),
-             SizedBox(height: 16,),
-          ],
-        ),
+        child: AddNotesForm(),
       ),
     );
   }
 }
 
-class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+class AddNotesForm extends StatefulWidget {
+  const AddNotesForm({
+    super.key,
+  });
+
+  @override
+  State<AddNotesForm> createState() => _AddNotesFormState();
+}
+
+class _AddNotesFormState extends State<AddNotesForm> {
+
+  final GlobalKey<FormState> formKey=GlobalKey();
+
+  AutovalidateMode autovalidateMode=AutovalidateMode.disabled;
+
+  String? title,subTitle;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-        color:kPrimaryColor,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: const Center(
-        child: Text('Add',style: TextStyle(color: Colors.black,fontSize: 20),),
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+         const SizedBox(height: 30),
+          CustomTextFiled(hint: 'Title',
+          onSave: (value)
+          {
+            title=value;
+          },),
+         const SizedBox(height: 16),
+          CustomTextFiled(hint: 'Content', maxLines: 5,
+          onSave: (value) {
+            subTitle=value;
+          },),
+          SizedBox(height: 16),
+          CustomButton(onTap: () {
+            if(formKey.currentState!.validate())
+            {
+              formKey.currentState!.save();
+            }
+            else
+            {
+              autovalidateMode=AutovalidateMode.always;
+
+              setState(() {
+                
+              });
+            }
+            
+          },),
+          SizedBox(height: 16),
+        ],
       ),
     );
   }
--- lib/widget/custom_Text_field.dart
+++ lib/widget/custom_Text_field.dart
@@ -2,14 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constant.dart';
 
 class CustomTextFiled extends StatelessWidget {
-  const CustomTextFiled({super.key, this.hint, this.maxLines=1});
+  const CustomTextFiled({super.key, this.hint, this.maxLines=1, this.onSave});
 
   final String? hint;
   final int? maxLines;
+  final void Function(String?)? onSave;
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSave,
+      validator: (value) {
+        if(value?.isEmpty ?? true)
+        {
+          return 'The filed is required';
+        }
+        else
+        {
+          return null;
+        }
+      },
       cursorColor: const Color.fromARGB(255, 111, 217, 203),
       //textAlign: TextAlign.center,
         textAlignVertical: TextAlignVertical.center,
